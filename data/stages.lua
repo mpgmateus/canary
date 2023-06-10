63f63f8a6cc96061ba978f361b23bb8dcc1eb281
Mateus <mpgmateus@gmail.com>
2023-06-10 13:14:37 -0300

    update stages.lua
    1st update on stages.lua

--- data/stages.lua
+++ data/stages.lua
@@ -4,72 +4,104 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7
+		maxlevel = 149,
+		multiplier = 150
 	}, {
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6
+		minlevel = 150,
+		maxlevel = 199,
+		multiplier = 130
 	}, {
-		minlevel = 21,
-		maxlevel = 50,
-		multiplier = 5
+		minlevel = 200,
+		maxlevel = 249,
+		multiplier = 120
 	}, {
-		minlevel = 51,
-		maxlevel = 100,
-		multiplier = 4
+		minlevel = 250,
+		maxlevel = 299,
+		multiplier = 115
 	}, {
-		minlevel = 101,
-		multiplier = 2
+		minlevel = 300,
+		maxlevel = 349,
+		multiplier = 110
+	}, {
+		minlevel = 350,
+		maxlevel = 399,
+		multiplier = 100
+	},{
+		minlevel = 400,
+		maxlevel = 449,
+		multiplier = 95
+	},{
+		minlevel = 450,
+		maxlevel = 499,
+		multiplier = 90
+	},{
+		minlevel = 500,
+		maxlevel = 549,
+		multiplier = 85
+	},{
+		minlevel = 550,
+		maxlevel = 599,
+		multiplier = 80
+	},{
+		minlevel = 600,
+		maxlevel = 999,
+		multiplier = 75
+	},{	
+		minlevel = 1000,
+		multiplier = 67
 	}
 }
 
 skillsStages = {
 	{
-		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15
+		minlevel = 1,
+		maxlevel = 100,
+		multiplier = 50
 	}, {
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 10
+		minlevel = 101,
+		maxlevel = 105,
+		multiplier = 30
 	}, {
-		minlevel = 81,
+		minlevel = 106,
 		maxlevel = 110,
-		multiplier = 6
+		multiplier = 20
 	}, {
 		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4
+		maxlevel = 115,
+		multiplier = 15
 	}, {
-		minlevel = 126,
-		multiplier = 2
+		minlevel = 116,
+		maxlevel = 120,
+		multiplier = 10
+	}, {	
+		minlevel = 121,
+		multiplier = 5
 	}
 }
 
 magicLevelStages = {
 	{
-		minlevel = 0,
-		maxlevel = 60,
-		multiplier = 10
-	}, {
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7
-	}, {
-		minlevel = 81,
+		minlevel = 1,
 		maxlevel = 100,
-		multiplier = 5
+		multiplier = 50
 	}, {
 		minlevel = 101,
+		maxlevel = 105,
+		multiplier = 30
+	}, {
+		minlevel = 106,
 		maxlevel = 110,
-		multiplier = 4
+		multiplier = 20
 	}, {
 		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3
+		maxlevel = 115,
+		multiplier = 15
 	}, {
-		minlevel = 126,
-		multiplier = 2
+		minlevel = 116,
+		maxlevel = 120,
+		multiplier = 10
+	}, {
+		minlevel = 121,
+		multiplier = 5
 	}
 }
